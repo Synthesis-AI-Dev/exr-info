c56d3d5703488b08c0264c8f11ebe64f59f23553
Shreeyak <ss.shrek7@gmail.com>
2021-01-08 21:41:58 +0530

    * Added reinhard tonemapping for optional method of converting Linear RGB to sRGB colorspace. * Added method to read multiple channels * Added methods to get cryptomatte channels and info * Refactored code, renamed methods.

--- exr_info/__init__.py
+++ exr_info/__init__.py
@@ -1,4 +1,4 @@
-
-from .exr_info import Renderer, CryptoLayerMapping, ExrChannels, ExrInfo, lin_rgb_to_srgb_colorspace
+from .exr_info import Renderer, CryptoLayerMapping, ExrChannels, ExrInfo
+from .exr_info import lin_rgb_to_srgb_colorspace, lin_rgb_to_srgb_reinhard_tonemapping
 
 __version__ = "1.0.1"
--- exr_info/exr_info.py
+++ exr_info/exr_info.py
@@ -4,12 +4,13 @@ import fnmatch
 from collections import namedtuple
 from dataclasses import dataclass
 from pathlib import Path
-from typing import Dict, List, Optional, Tuple
+from typing import Iterator, List, Optional, Tuple
 
 import Imath
 import numpy as np
 import OpenEXR
 
+import cv2
 
 CryptoDef = namedtuple("CryptoDef", ["name", "id"])
 
@@ -22,6 +23,17 @@ class Renderer(enum.Enum):
 class ExrDtype(enum.Enum):
     FLOAT32 = 0
     FLOAT16 = 1
+
+
+pixel_dtype = {
+    ExrDtype.FLOAT32: Imath.PixelType(Imath.PixelType.FLOAT),
+    ExrDtype.FLOAT16: Imath.PixelType(Imath.PixelType.HALF),
+}
+
+numpy_dtype = {
+    ExrDtype.FLOAT32: np.float32,
+    ExrDtype.FLOAT16: np.float16,
+}
 
 
 @dataclass
@@ -201,11 +213,18 @@ class ExrInfo:
         """
         self.exr_file = exr_file
         self.header = exr_file.header()
+
+        # Byte-strings cause problems when parsing:
+        for key, val in self.header.items():
+            if isinstance(val, bytes):
+                self.header[key] = str(val, 'utf-8')
+
         dw = self.header["dataWindow"]
 
         self.height = int(dw.max.y - dw.min.y + 1)
         self.width = int(dw.max.x - dw.min.x + 1)
-        self.channels = list(self.header["channels"].keys())
+        self.channels_dict = self.header["channels"]
+        self.channels = list(self.channels_dict.keys())
 
         self.renderer = self.identify_render_engine()
 
@@ -234,20 +253,22 @@ class ExrInfo:
         crypto_defs = self.get_cryptomatte_definitions()
         if len(crypto_defs) == 0:
             return False
-
         return True
 
     def get_cryptomatte_definitions(self) -> List[CryptoDef]:
-        """Get the name and ID of all cryptomatte definitions present in EXR
+        r"""Get the name and ID of all cryptomatte definitions present in EXR
 
         A render can contain multiple cryptomattes (each is referred to as a cryptomatte definition). A render engine
         can base each cryptomatte definition on a different criteria, such as material or object id.
+
         Each cryptomatte definition is given a name and a 7-character ID (which is a hash of the name). The channels
         corresponding to each cryptomatte definition within the EXR will append 2 digits to the cryptomatte name.
 
-        Note: The number of cryptomatte channels (per definition) depends on the "level" of the cryptomatte. The level
-              denotes the number of objects a cryptomatte can represent. The default level of 6 results in 3 cryptomatte
-              layers (00, 01, 02) in the EXR. Each layer is represented by a channel group with RGBA channels.
+        Note:
+            The number of cryptomatte channels (per definition) depends on the "level" of the cryptomatte. The level
+            denotes the number of objects a cryptomatte can represent. The default level of 6 results in 3 cryptomatte
+            layers (00, 01, 02) in the EXR. Each layer is represented by a group of 4 channels (R, G, B, A).
+
                 ``num_layers = math.ceil(level / 2)``
 
         Returns:
@@ -276,6 +297,7 @@ class ExrInfo:
 
                 ...
                 VRayCryptomatte_Mtl00.A: FLOAT (1, 1)
+                VRayCryptomatte_Mtl00.B: FLOAT (1, 1)
                 .
                 .
                 VRayCryptomatte_Mtl02.R: FLOAT (1, 1)
@@ -290,6 +312,38 @@ class ExrInfo:
             crypto_defs.append(CryptoDef(name=crypto_name, id=crypto_id))
 
         return crypto_defs
+
+    def get_cryptomatte_channels(self, crypto_def: CryptoDef):
+        """Extract list of all the cryptomatte channels, sorted by layer and in order RGBA per layer.
+        The ordered channels are used to extract the object masks from a cryptomatte.
+
+        Usage:
+            >>> self.get_cryptomatte_channels(CryptoDef(name='crypto', id='xxxxxxx'))
+            [crypto00.R, crypto00.G, crypto00.B, crypto00.A, crypto01.R, crypto01.G, ..., crypto02.A]
+        """
+        wildcard = f"{crypto_def.name}??.?"  # Example: cryptomatte00.R
+        crypto_channels = fnmatch.filter(self.channels, wildcard)
+
+        # Crypto channels should be in order RGBA
+        crypto_channels = sorted(crypto_channels, key=self._channel_sort_key)
+
+        return crypto_channels
+
+    @staticmethod
+    def _channel_sort_dictionary(key):
+        if key == 'R' or key == 'r':
+            return "0000000001"
+        elif key == 'G' or key == 'g':
+            return "0000000002"
+        elif key == 'B' or key == 'b':
+            return "0000000003"
+        elif key == 'A' or key == 'a':
+            return "0000000004"
+        else:
+            return key
+
+    def _channel_sort_key(self, i):
+        return [self._channel_sort_dictionary(x) for x in i.split(".")]
 
     def is_vray_denoise_present(self) -> bool:
         """Check whether the image was created using Vray denoising.
@@ -327,35 +381,73 @@ class ExrInfo:
     def get_channels_str(self):
         """Get the list of channels as a string for printing"""
         channels_str = "Channels: \n"
-        channels_dict = self.header["channels"]
-        for _key in channels_dict:
-            channels_str += f"  {_key}: {channels_dict[_key].type}\n"
+        for _key in self.channels_dict:
+            channels_str += f"  {_key}: {self.channels_dict[_key].type}\n"
 
         return channels_str
 
-    def exr_channel_to_numpy(self, channel_name: str, dtype: ExrDtype = ExrDtype.FLOAT32) -> np.ndarray:
+    def get_channel_precision(self, channel_name: str) -> ExrDtype:
+        """Get the precision of a channel within the EXR"""
+        if channel_name not in self.channels:
+            raise TypeError(
+                f"There is no channel called '{channel_name}' in EXR file. "
+                f"Available channels:\n{self.get_channels_str()}"
+            )
+
+        # Use values in dict to find matching key (values are unique)
+        for exr_d, pix_d in pixel_dtype.items():
+            prec = self.channels_dict[channel_name].type
+            if prec == pix_d:
+                return exr_d
+
+    def read_channel(self, channel_name: str, cast_dtype: Optional[ExrDtype] = None) -> np.ndarray:
         """Extracts a channel in an EXR file into a numpy array
 
         Args:
             channel_name (str): The name of the channel to be converted to numpy
-            dtype (ExrDtype): Whether the data in channel is of float32 or float16 type
+            cast_dtype (ExrDtype): If given, cast array to given dtype. Else, dtype inferred from information in header
 
         Returns:
-            numpy.ndarray: The extracted channel in form of numpy array with dtype as specified in input parameter.
-                           Shape: (H, W).
+            numpy.ndarray: The extracted channel in form of numpy array. Shape: (H, W).
         """
-        if dtype == ExrDtype.FLOAT32:
-            point_type = Imath.PixelType(Imath.PixelType.FLOAT)
-            np_type = np.float32
-        else:
-            point_type = Imath.PixelType(Imath.PixelType.HALF)
-            np_type = np.float16
+        chan_dtype = self.get_channel_precision(channel_name)
+        np_type = numpy_dtype[chan_dtype]
+        channel_arr = np.frombuffer(self.exr_file.channel(channel_name), dtype=np_type)
+        channel_arr = channel_arr.reshape((self.height, self.width))
 
-        channel_arr = np.frombuffer(self.exr_file.channel(channel_name, point_type), dtype=np_type)
-        height, width = self.get_imsize()
-        channel_arr = channel_arr.reshape((height, width))
+        if cast_dtype is not None:
+            channel_arr = channel_arr.astype(np_type[cast_dtype])
 
         return channel_arr
+
+    def read_channels(self, channel_names: Iterator[str], cast_dtype: Optional[ExrDtype] = None) -> List[np.ndarray]:
+        """Read a list of channels from EXR into numpy arrays
+
+        Args:
+            channel_names: An iterable containing the names of the channels to be read
+            cast_dtype (ExrDtype): If given, cast array to given dtype. Else, dtype inferred from information in header
+
+        Returns:
+            list(np.ndarray): A list of the extracted channels in numpy, with dtype inferred from information in header.
+                              Shape: [H, W]
+        """
+        channel_dtypes = []
+        for channel_name in channel_names:
+            channel_dtypes.append(self.get_channel_precision(channel_name))
+
+        channels_tup = self.exr_file.channels(channel_names)
+
+        channels_list = []
+        for chan_name, chan_str, chan_dtype in zip(channel_names, channels_tup, channel_dtypes):
+            np_type = numpy_dtype[chan_dtype]
+            channel_arr = np.frombuffer(self.exr_file.channel(chan_name), dtype=np_type)
+            channel_arr = channel_arr.reshape((self.height, self.width))
+            if cast_dtype is not None:
+                channel_arr = channel_arr.astype(np_type[cast_dtype])
+
+            channels_list.append(channel_arr)
+
+        return channels_list
 
 
 def lin_rgb_to_srgb_colorspace(img_lin_rgb: np.ndarray):
@@ -374,6 +466,7 @@ def lin_rgb_to_srgb_colorspace(img_lin_rgb: np.ndarray):
     References:
         Formula for conversion from linear RGb to sRGB colorspace:
             - https://blender.stackexchange.com/questions/65288/convert-openexr-float-to-color-value
+            - https://stackoverflow.com/a/12894053/6021054
         Blender Color Management:
             - https://docs.blender.org/manual/en/latest/render/color_management.html
             "File formats such as PNG or JPEG will typically store colors in a color space ready for display (sRGB).
@@ -391,3 +484,26 @@ def lin_rgb_to_srgb_colorspace(img_lin_rgb: np.ndarray):
     img_srgb = np.where(img_lin_rgb <= 0.0031308, img_lin_rgb * 12.92, 1.055 * np.power(img_lin_rgb, 1 / 2.4) - 0.055)
 
     return img_srgb
+
+
+def lin_rgb_to_srgb_reinhard_tonemapping(
+    img_float_lin: np.ndarray,
+    gamma: float = 0.454,
+    intensity: float = 2,
+    light_adapt: float = 0,
+    color_adapt: float = 1,
+):
+    """Uses Reinhard method for Tone Mapping (i.e. Color Mapping) to convert Linear RGB to sRGB
+
+    Args:
+        img_float_lin: RGB image. Shape: [H, W, 3]
+        gamma: gamma value for gamma correction. See createTonemap
+        intensity: result intensity in [-8, 8] range. Greater intensity produces brighter results.
+        light_adapt: light adaptation in [0, 1] range. If 1 adaptation is based only on pixel
+            value, if 0 it's global, otherwise it's a weighted mean of this two cases.
+        color_adapt: chromatic adaptation in [0, 1] range. If 1 channels are treated independently,
+            if 0 adaptation level is the same for each channel.
+    """
+    tonemapReinhard = cv2.createTonemapReinhard(gamma, intensity, light_adapt, color_adapt)
+    ldr_img = tonemapReinhard.process(img_float_lin)
+    return ldr_img
